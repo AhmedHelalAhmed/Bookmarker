f2693b486cd18abdb9c80525678de8b4c47e7ebf
Ahmed Abd Elfatah <chem.Ahmedaf@gmail.com>
2020-05-14 01:07:31 +0200

    My Bookmarker

--- js/assignment_three.js
+++ js/assignment_three.js
@@ -1,109 +1,4 @@
 
-
-// var siteName=document.getElementById("siteName");
-// var siteUrl=document.getElementById("siteUrl");
-
-// var sitesContainer;
-
-// if (localStorage.getItem("mysite")==null)
-// {
-//     sitesContainer= [];
-// }
-// else
-// {
-//     sitesContainer= JSON.parse( localStorage.getItem("mysite") );
-//     show(); 
-// }
-
-// function add()
-// {
- 
-//     var mySites =
-//     {
-//        name:siteName.value,
-//        url:siteUrl.value
-//     }
-
-//     sitesContainer.push(mySites);
-
-//  if(mySites.name=="" && mySites.url=="")
-//  {
-//       nameError();
-//       urlError();
-//  }
-
-//   else if(mySites.name=="")
-//   {
-//     nameError();
-//   }
-//    else if(mySites.url=="")
-//   {
-//     urlError();
-//   }
-
-//   else
-//  {
-
-//     localStorage.setItem("mysite" , JSON.stringify(sitesContainer))
-
-//  show();
-
-//  }
-
-      
-// }
-
-
-// function show()
-// {
-
-// var col="";
-
-// for(var i=0;i<sitesContainer.length;i++) 
-// {
-//   col+=`<div class="text-center mt-4 p-3 d-flex">` +sitesContainer[i].name+
-//    `<button onclick="visitSite(`+i+`)"  class=" btn btn-info ml-5 ">visit</button> 
-//    <button onclick="deleteSite(`+i+`)" class="btn btn-danger ml-2">Delete</button> 
-//    </div>`
-// }
-
-// document.getElementById("button-container").innerHTML=col;
-// }
-
-
-
-// function nameError()
-// {
-//   document.getElementById("nameError").innerHTML="Name is required";
-// }
-
-// function urlError()
-// {
-//   document.getElementById("urlError").innerHTML="Url Field is required";
-// }
-
-
-// function deleteSite(index)
-// {
-//     sitesContainer.splice(index,1);
-//     localStorage.setItem("mysite" ,JSON.stringify ( sitesContainer ) )
-//    show();
-// }
-
-// function visitSite(index)
-// {
-   
-//   var visit=` <a href="`+sitesContainer[index].url+`" target="_blank">visit</a> `
-//   document.getElementById("link").innerHTML=visit;
-  
-// }
-
-
-
-
-
-/// start from here
-
  
 var siteName=document.getElementById("siteName");
 var siteUrl=document.getElementById("siteUrl");
@@ -120,7 +15,7 @@ submitBtn.onclick=function()
     if(siteName.value=="" && siteUrl.value=="")
     {
         nameError.innerHTML="Name is required" ;
-        urlError.innerHTML="Url Field is required"
+        urlError.innerHTML="Url Field is required";
     }
 
     else if(siteName.value=="")
@@ -188,7 +83,7 @@ col="";
 for(var i=0;i<siteContainer.length;i++)
 {
  
-    col+=` <div class="row mb-3 py-4 bg-color  ">
+    col+=` <div class="row my-4 py-4 bg-color  ">
     <div class="col-sm-6">
 
         <div class="row ">
